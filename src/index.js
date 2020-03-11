fc9e260598d747b946ac772b51a69cc0bc59f56e
Erin Kang <erin@apposter.com>
2020-03-11 15:22:06 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const reducer = (count = 0) => { 
+  return count;
 };
 
-const handleAdd = () => {
-  console.log('Add');
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  console.log('Minus');
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+const countStore = createStore(reducer);
+console.log(countStore.getState())
