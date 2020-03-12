7a94d2715ab716dc00b6e2afaf0a2cb400cbd53a
Erin Kang <erin@apposter.com>
2020-03-12 12:19:48 +0900

    Vanila ToDo

--- src/index.js
+++ src/index.js
@@ -1,43 +1,31 @@
 import { createStore } from 'redux';
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-number.innerText = 0;
+const ADD_TODO = 'ADD';
+const DELETE_TODO = 'DELETE';
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-  switch( action.type ) {
-    case ADD:
-      return count+1;
-    case MINUS :
-      return count-1;
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch(action.type == ADD_TODO) {
+    case 'ADD':
+      return [];
+    case 'DELETE':
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  console.log("there was a change on the store");
-  console.log(countStore.getState());
-  number.innerText = countStore.getState();
+const onSubmit = e =>{
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  store.dispatch({type: ADD_TODO, text: toDo})
 }
 
-console.log(countStore)
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({type: ADD}) // type은 바꿀 수 없다. reducer(countModifier) action 값의 필수.
-};
-
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS})
-};
-
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+form.addEventListener('submit', onSubmit);
