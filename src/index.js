71e2c715cf2daee8c474ab3953f091d1d6e5c929
Erin Kang <erin@apposter.com>
2020-03-12 17:11:45 +0900

    Delete To Do

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const ADD_TODO = 'ADD';
 const DELETE_TODO = 'DELETE';
 
 const reducer = (state = [], action) => {
+  console.log(state, action)
   switch(action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text, id: Date.now()}]; // return new state! es6: spread! (original state 변형 불가)
+      return [{text: action.text, id: Date.now()}, ...state]; // return new state! es6: spread! (original state 변형 불가)
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -21,11 +22,52 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()))
 
+const paintToDos = () => {
+  const toDos = store.getState();
+  console.log(toDos);
+  ul.innerHTML='';
+  toDos.forEach( toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.addEventListener('click', dispatchDeleteToDo);
+    btn.innerText = 'DEL';
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+store.subscribe(paintToDos);
+
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO, id
+  };
+}
+
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text))
+};
+
+const dispatchDeleteToDo = (e) => {
+  console.log(e.target.parentNode); //id
+  const id = parseInt(e.target.parentNode.id);
+  console.log(id);
+  store.dispatch(deleteToDo(id));
+}
+
 const onSubmit = e =>{
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({type: ADD_TODO, text: toDo})
+  dispatchAddToDo(toDo);
 }
 
 form.addEventListener('submit', onSubmit);
