50dbd779762ef8c4fce75a70a3dbd3e062e7aeac
Erin Kang <erin@apposter.com>
2020-03-12 12:04:12 +0900

    recap refactor

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const number = document.querySelector('span');
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count+1;
-  } else if (action.type === 'MINUS') {
-    return count-1;
+  switch( action.type ) {
+    case ADD:
+      return count+1;
+    case MINUS :
+      return count-1;
+    default:
+      return count;
   }
-  return count;
 };
 
 const countStore = createStore(countModifier);
@@ -27,11 +32,11 @@ console.log(countStore)
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({type:'ADD'})
+  countStore.dispatch({type: ADD}) // type은 바꿀 수 없다. reducer(countModifier) action 값의 필수.
 };
 
 const handleMinus = () => {
-  countStore.dispatch({type: 'MINUS'})
+  countStore.dispatch({type: MINUS})
 };
 
 add.addEventListener('click', handleAdd)
