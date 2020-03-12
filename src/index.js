fcf34b768c033137ff4160c81ba4d14a928acd7c
Erin Kang <erin@apposter.com>
2020-03-12 11:58:07 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const reducer = (count = 0, action) => {
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
   if (action.type === 'ADD') {
     return count+1;
   } else if (action.type === 'MINUS') {
@@ -13,12 +15,24 @@ const reducer = (count = 0, action) => {
   return count;
 };
 
-const countStore = createStore(reducer);
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'ADD'});
-countStore.dispatch({type: 'MINUS'});
+const countStore = createStore(countModifier);
 
-console.log(countStore.getState()) //result : 4
+const onChange = () => {
+  console.log("there was a change on the store");
+  console.log(countStore.getState());
+  number.innerText = countStore.getState();
+}
+
+console.log(countStore)
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type:'ADD'})
+};
+
+const handleMinus = () => {
+  countStore.dispatch({type: 'MINUS'})
+};
+
+add.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
